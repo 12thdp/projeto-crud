5db83d3719b33ee395a8951ea2b3edd5c4ce572f
12thdp <thierry.pereira2@aluno.ce.gov.br>
2025-04-12 17:07:32 -0300

    Bootstrap instalado, componentes de formulário e tabela criados

--- backend/produtos/src/main/java/br/com/crud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/crud/produtos/controle/ProdutoControle.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.crud.produtos.modelo.ProdutoModelo;
 import br.com.crud.produtos.modelo.RespostaModelo;
 import br.com.crud.produtos.servico.ProdutoServico;
